9299a04fc01d1fe339327540f6666235fc3c205e
k-srujana <mailfromsrujana@gmail.com>
2026-04-20 13:00:51 +0530

    added variables.tf

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -1,3 +1,6 @@
 variable "ami" {}
 variable "instance_type" {}
 variable "instance_name" {}
+variable "vpc_cidr" {}
+variable "public_subnet" {}
+variable "private_subnet" {}
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "private_subnet_id" {
+  type = string
+}
+
+variable "iam_role" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
