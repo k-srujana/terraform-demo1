64cfe5d5f2e5468f5d13650496fdd165ac63c7a8
k-srujana <mailfromsrujana@gmail.com>
2026-04-20 13:28:16 +0530

    hi

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -6,8 +6,8 @@ module "vpc" {
   source = "../../modules/vpc"
 
   vpc_cidr        = "10.0.0.0/16"
-  public_subnet   = "10.0.1.0/24"
-  private_subnet  = "10.0.2.0/24"
+  public_subnet_ids = [subnet1, subnet2]
+  private_subnet  = "10.0.3.0/24"
 }
 
 module "ec2" {
